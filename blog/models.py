ec87fb84c3c8e3ce73941e23e692bbca756f81ba
Chris Diggle <chrisdiggle8@outlook.com>
2024-02-11 17:59:31 +0000

    reverted changes

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
-
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -20,22 +20,23 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
+    approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
+
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
 
